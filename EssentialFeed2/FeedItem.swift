48731b590e41321eda92cee8e5fb390310d90a4e
Zsivics Sanel <sanel.zsivics@kh.hu>
2021-01-03 20:56:45 +0100

    Create the FeedItem model

---
+++ EssentialFeed2/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed2
+//
+//  Created by mbp01 on 2021. 01. 03..
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
