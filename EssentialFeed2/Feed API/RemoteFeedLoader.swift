ea652df2294f65d18d651f372ffdeb769f35332d
Zsivics Sanel <sanel.zsivics@kh.hu>
2021-01-04 23:34:14 +0100

    Move `RemoteFeedLoader` components to production

---
+++ EssentialFeed2/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed2
+//
+//  Created by mbp01 on 2021. 01. 04..
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
