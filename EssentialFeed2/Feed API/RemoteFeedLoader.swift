0e25f0e7b0274f3823b94bab7a4682aedc4c5584
Zsivics Sanel <sanel.zsivics@kh.hu>
2021-01-10 15:36:53 +0100

    Delivers empty items array on 200 HTTP response with JSON empty list

--- EssentialFeed2/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed2/Feed API/RemoteFeedLoader.swift
@@ -36,10 +36,14 @@ public final class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { result in
-            switch result {
-            case .success:
-                completion(.failure(.invalidData))
+        client.get(from: url) { clientResult in
+            switch clientResult {
+            case let .success(data, _):
+                if let _ = try? JSONSerialization.jsonObject(with: data) {
+                    completion(.success([]))
+                } else {
+                    completion(.failure(.invalidData))
+                }
             case .failure:
                 completion(.failure(.connectivity))
             }
