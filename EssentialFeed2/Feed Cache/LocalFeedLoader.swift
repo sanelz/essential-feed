a5a5411a506880a69a985bdfe900e02da4d2b4aa
Zsivics Sanel <zsivics.sanel@gmail.com>
2021-02-22 19:57:41 +0100

    Replace load command completion to return a result type, rather than an optional error

--- EssentialFeed2/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed2/Feed Cache/LocalFeedLoader.swift
@@ -6,6 +6,7 @@ public final  class LocalFeedLoader {
     private let currentDate: () -> Date
 
     public typealias SaveResult = Error?
+    public typealias LoadResult = LoadFeedResult
 
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
@@ -24,8 +25,12 @@ public final  class LocalFeedLoader {
         }
     }
 
-    public func load(completion: @escaping (Error?) -> Void) {
-        store.retrieve(completion: completion)
+    public func load(completion: @escaping (LoadResult) -> Void) {
+        store.retrieve { error in
+            if let error = error {
+                completion(.failure(error))
+            } 
+        }
     }
 
     // MARK: - Helpers
