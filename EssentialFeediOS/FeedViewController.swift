0498a10d5ee09d56a6c8c9c974620d1177e560b1
Zsivics Sanel <zsivics.sanel@gmail.com>
2021-04-05 22:50:31 +0200

    Feed image view retry button is visible on image url load error

--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -59,11 +59,13 @@ final public class FeedViewController: UITableViewController {
         cell.locationLabel.text = cellModel.location
         cell.descriptionLabel.text = cellModel.description
         cell.feedImageView.image = nil
+        cell.feedImageRetryButton.isHidden = true
         cell.feedImageContainer.startShimmering()
         tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
-            cell?.feedImageContainer.stopShimmering()
             let data = try? result.get()
             cell?.feedImageView.image = data.map(UIImage.init) ?? nil
+            cell?.feedImageRetryButton.isHidden = (data != nil)
+            cell?.feedImageContainer.stopShimmering()
         }
         return cell
     }
