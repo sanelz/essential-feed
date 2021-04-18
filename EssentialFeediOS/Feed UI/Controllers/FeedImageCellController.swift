2ab6f7f3cb81531daa129799856f2ae925bf64f8
Zsivics Sanel <zsivics.sanel@gmail.com>
2021-04-18 19:40:42 +0200

    Add `FeedImagePresenter` holding `FeedImage` presentatino logic (including image loading state)

---
+++ EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -0,0 +1,45 @@
+//
+//  FeedImageViewCellController.swift
+//  EssentialFeediOS
+//
+//  Created by mbp01 on 2021. 04. 08..
+//
+
+import UIKit
+
+protocol FeedImageCellControllerDelegate {
+    func didRequestImage()
+    func didCancelImageRequest()
+}
+
+final class FeedImageCellController: FeedImageView {
+    private let delegate: FeedImageCellControllerDelegate
+    private lazy var cell = FeedImageCell()
+
+    init(delegate: FeedImageCellControllerDelegate) {
+        self.delegate = delegate
+    }
+
+    func view() -> UITableViewCell {
+        delegate.didRequestImage()
+        return cell
+    }
+
+    func preload() {
+        delegate.didRequestImage()
+    }
+
+    func cancelLoad() {
+        delegate.didCancelImageRequest()
+    }
+
+    func display(_ viewModel: FeedImageViewModel<UIImage>) {
+        cell.locationContainer.isHidden = !viewModel.hasLocation
+        cell.locationLabel.text = viewModel.location
+        cell.descriptionLabel.text = viewModel.description
+        cell.feedImageView.image = viewModel.image
+        cell.feedImageContainer.isShimmering = viewModel.isLoading
+        cell.feedImageRetryButton.isHidden = !viewModel.shouldRetry
+        cell.onRetry = delegate.didRequestImage
+    }
+}
