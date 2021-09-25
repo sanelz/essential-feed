a12837b8cdbdc7451f1e8ce80de3f1f2b4a9163b
Zsivics Sanel <zsivics.sanel@gmail.com>
2021-09-26 00:09:54 +0200

    `LocalFeedImageDataLoader.saveImageDataForURL` requests image data insertion for url into the store

--- EssentialFeed2Tests/Feed Cache/LocalFeedImageDataLoaderTests.swift
+++ EssentialFeed2Tests/Feed Cache/LocalFeedImageDataLoaderTests.swift
@@ -16,6 +16,16 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
         XCTAssertTrue(store.receivedMessages.isEmpty)
     }
 
+    func test_saveImageDataForURL_requestsImageDataInsertionForURL() {
+        let (sut, store) = makeSUT()
+        let url = anyURL()
+        let data = anyData()
+
+        sut.save(data, for: url) { _ in }
+
+        XCTAssertEqual(store.receivedMessages, [.insert(data: data, for: url)])
+    }
+
     func test_loadImageDataFromURL_requestsStoreDataForURL() {
         let (sut, store) = makeSUT()
         let url = anyURL()
@@ -127,12 +137,18 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
     }
 
     private class StoreSpy: FeedImageDataStore {
+
         enum Messages: Equatable {
+            case insert(data: Data, for: URL)
             case retrieve(dataFor: URL)
         }
 
         private var completions = [(FeedImageDataStore.Result) -> Void]()
         private(set) var receivedMessages = [Messages]()
+
+        func insert(data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
+            receivedMessages.append(.insert(data: data, for: url))
+        }
 
         func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.Result) -> Void) {
             receivedMessages.append(.retrieve(dataFor: url))
