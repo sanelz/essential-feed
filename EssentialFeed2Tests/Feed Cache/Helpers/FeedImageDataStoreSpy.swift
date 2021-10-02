20852b2e646dc66bb9325837cd51bdf9e62ebab3
Zsivics Sanel <zsivics.sanel@gmail.com>
2021-10-02 08:53:43 +0200

    `LocalFeedImageDataLoader.saveImageDataForURL` requests image data insertion into store

--- EssentialFeed2Tests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
+++ EssentialFeed2Tests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
@@ -17,9 +17,11 @@ class FeedImageDataStoreSpy: FeedImageDataStore {
 
     private(set) var receivedMessages = [Messages]()
     private var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
+    private var insertionCompletions = [(FeedImageDataStore.InsertionResult) -> Void]()
 
     func insert(data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
         receivedMessages.append(.insert(data: data, for: url))
+        insertionCompletions.append(completion)
     }
 
     func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
@@ -33,5 +35,9 @@ class FeedImageDataStoreSpy: FeedImageDataStore {
 
     func completeRetrieval(with data: Data?, at index: Int = 0) {
         retrievalCompletions[index](.success(data))
+    }
+
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](.failure(error))
     }
 }
