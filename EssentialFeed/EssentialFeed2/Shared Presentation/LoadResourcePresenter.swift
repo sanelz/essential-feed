2881b3cd8ea7d03f6681ddb6dae9d75f01032997
Sanel Zsivics <zsivics.sanel@gmail.com>
2022-05-18 12:14:06 +0200

    Rename method

--- EssentialFeed/EssentialFeed2/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/EssentialFeed2/Shared Presentation/LoadResourcePresenter.swift
@@ -18,7 +18,7 @@ public final class LoadResourcePresenter {
         self.loadingView = loadingView
     }
 
-    public func didStartLoadingFeed() {
+    public func didStartLoading() {
         errorView.display(.noError)
         loadingView.display(FeedLoadingViewModel(isLoading: true))
     }
