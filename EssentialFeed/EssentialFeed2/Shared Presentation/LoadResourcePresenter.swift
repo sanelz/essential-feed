3830ffe1afab90bcb5b0d1971bed2c118e009d82
Sanel Zsivics <zsivics.sanel@gmail.com>
2022-05-18 12:38:51 +0200

    Displays mapped resource on successful resource loading

--- EssentialFeed/EssentialFeed2/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/EssentialFeed2/Shared Presentation/LoadResourcePresenter.swift
@@ -1,10 +1,17 @@
 import Foundation
 
+public protocol ResourceView {
+    func display(_ viewModel: String)
+}
+
 public final class LoadResourcePresenter {
-    private let feedView: FeedView
+    public typealias Mapper = (String) -> String
+    
+    private let resourceView: ResourceView
     private let loadingView: FeedLoadingView
     private let errorView: FeedErrorView
-
+    private let mapper: Mapper
+    
     private var feedLoadError: String {
         NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
                           tableName: "Feed",
@@ -12,10 +19,11 @@ public final class LoadResourcePresenter {
                           comment: "Error message displayed when we can't load the image feed from the server")
     }
 
-    public init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
-        self.feedView = feedView
+    public init(resourceView: ResourceView, loadingView: FeedLoadingView, errorView: FeedErrorView, mapper: @escaping Mapper) {
+        self.resourceView = resourceView
         self.errorView = errorView
         self.loadingView = loadingView
+        self.mapper = mapper
     }
 
     public func didStartLoading() {
@@ -23,8 +31,8 @@ public final class LoadResourcePresenter {
         loadingView.display(FeedLoadingViewModel(isLoading: true))
     }
 
-    public func didFinishLoadingFeed(with feed: [FeedImage]) {
-        feedView.display(FeedViewModel(feed: feed))
+    public func didFinishLoading(with resource: String) {
+        resourceView.display(mapper(resource))
         loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
 
