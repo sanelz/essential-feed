ddb6bced459d53d130476b3b534a01151fa105d2
Sanel Zsivics <zsivics.sanel@gmail.com>
2022-05-19 11:58:14 +0200

    Rename and move ResourceLoadingView and ViewModel to Shared module

--- EssentialFeed/EssentialFeed2Tests/Shared Presentation/LoadResourcePresenterTests.swift
+++ EssentialFeed/EssentialFeed2Tests/Shared Presentation/LoadResourcePresenterTests.swift
@@ -70,7 +70,7 @@ class LoadResourcePresenterTests: XCTestCase {
         return value
     }
 
-    private class ViewSpy: ResourceView, FeedLoadingView, FeedErrorView {
+    private class ViewSpy: ResourceView, ResourceLoadingView, FeedErrorView {
         typealias ResourceViewModel = String
         
         enum Message: Hashable {
@@ -85,7 +85,7 @@ class LoadResourcePresenterTests: XCTestCase {
             messages.insert(.display(errorMessage: viewModel.errorMessage))
         }
 
-        func display(_ viewModel: FeedLoadingViewModel) {
+        func display(_ viewModel: ResourceLoadingViewModel) {
             messages.insert(.display(isLoading: viewModel.isLoading))
         }
 
