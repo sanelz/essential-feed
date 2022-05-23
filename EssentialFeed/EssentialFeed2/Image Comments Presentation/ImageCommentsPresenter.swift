915b31fbbb26d5024f0eca286430a76bb3f04c9c
Sanel Zsivics <zsivics.sanel@gmail.com>
2022-05-23 16:42:24 +0200

    Inject currentDate, locale and calendar to make tests reliable in any locale

--- EssentialFeed/EssentialFeed2/Image Comments Presentation/ImageCommentsPresenter.swift
+++ EssentialFeed/EssentialFeed2/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -24,13 +24,20 @@ public final class ImageCommentsPresenter {
                           comment: "Title for the image comments view")
     }
     
-    public static func map(_ imageComments: [ImageComment]) -> ImageCommentsViewModel {
+    public static func map(
+        _ imageComments: [ImageComment],
+        currentDate: Date = Date(),
+        calendar: Calendar = .current,
+        locale: Locale = .current
+    ) -> ImageCommentsViewModel {
         let formatter = RelativeDateTimeFormatter()
+        formatter.calendar = calendar
+        formatter.locale = locale
         
         return ImageCommentsViewModel(comments: imageComments.map { comment in
             ImageCommentViewModel(
                 message: comment.message,
-                date: formatter.localizedString(for: comment.createdAt, relativeTo: Date()),
+                date: formatter.localizedString(for: comment.createdAt, relativeTo: currentDate),
                 username: comment .username)
         })
     }
--- EssentialFeed/EssentialFeed2Tests/Image Comments Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeed2Tests/Image Comments Presentation/ImageCommentsPresenterTests.swift
@@ -9,6 +9,8 @@ class ImageCommentsPresenterTests: XCTestCase {
     
     func test_map_createsViewModel() {
         let now = Date()
+        let calendar = Calendar(identifier: .gregorian)
+        let locale = Locale(identifier: "en_US_POSIX")
         
         let comments = [
             ImageComment(
@@ -24,7 +26,12 @@ class ImageCommentsPresenterTests: XCTestCase {
 
         ]
         
-        let viewModel = ImageCommentsPresenter.map(comments)
+        let viewModel = ImageCommentsPresenter.map(
+            comments,
+            currentDate: now,
+            calendar: calendar,
+            locale: locale
+        )
         
         XCTAssertEqual(viewModel.comments, [
             ImageCommentViewModel(
