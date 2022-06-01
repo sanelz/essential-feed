ca98b18ff7d108a9dd6b350b64ce82dca5980bf0
Sanel Zsivics <zsivics.sanel@gmail.com>
2022-06-01 16:58:03 +0200

    Replace protocol composition in a single type with a struct composition to prevent forcing clients to implement methods they don't care about

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -36,7 +36,7 @@ final class FeedViewAdatper: ResourceView {
                 errorView: WeakRefVirtualProxy(view),
                 mapper: UIImage.tryMake)
             
-            return view
+            return CellController(view)
         })
     }
 }
